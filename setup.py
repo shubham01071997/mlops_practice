30be6f65066cbde72c463e1a737fac638545f81b
Shubham <gaikwadshubham65@gmail.com>
2024-02-22 23:01:28 +0530

    mlops code added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+setup(
+    name='DimondPricePrediction',
+    version='0.0.1',
+    author='shubham gaikwad',
+    author_email='gaikwadshubham65@gmail.com',
+    packages=find_packages()
+)
